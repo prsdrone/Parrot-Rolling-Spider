f678bd7d1087c804164b79f8ec02cd68cc977134
HarunBerk16 <hrnbrkisk@gmail.com>
2020-09-02 23:32:43 +0300

    v4 previous version compatible

--- Models/Values - Variables/initialConditions.m
+++ Models/Values - Variables/initialConditions.m
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 %%%VARIABLE SS SELECTORS
 
 %Airframe Variable SS Selector
@@ -6,3 +7,13 @@ VSS_AIRFRAME = 1;
 
 %Environment Variable SS Selector
 VSS_ENV = 1;
+=======
+%%%VARIABLE SS SELECTORS
+
+%Airframe Variable SS Selector
+VSS_AIRFRAME = 1;
+
+
+%Environment Variable SS Selector
+VSS_ENV = 1;
+>>>>>>> refs/remotes/origin/master
