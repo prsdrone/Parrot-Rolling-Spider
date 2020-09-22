e7d0cd9fbc15869bc3b8c5a3f24eb02dd5cb3891
Fatih <Fatih@DESKTOP-L5OU2BG>
2020-09-22 18:56:11 +0300

    Equations of Motion Done

---
+++ Models/Values - Variables/Airframe Values/AFVars.m
@@ -0,0 +1,12 @@
+initial.Pos = [0;0;-0.5];
+initial.Ve = [0;0;0];
+initial.Vbdot = [0;0;0];
+initial.wb = [0;0;0];
+initial.Euler = [0;0;0];
+initial.wbdot = [0;0;0];
+initial.Vb = [0;0;0];
+
+
+inertia = [5.82857e-05;7.16914e-05;1.e-04];
+mass = 0.063;
+g = 9.81;
--- Models/Values - Variables/initialConditions.m
+++ Models/Values - Variables/initialConditions.m
@@ -6,5 +6,3 @@ VSS_AIRFRAME = 1;
 
 %Environment Variable SS Selector
 VSS_ENV = 1;
-=======
->>>>>>> 9567f8d7fbb6ef64d00a333e9ea88ec5ca1085c5
